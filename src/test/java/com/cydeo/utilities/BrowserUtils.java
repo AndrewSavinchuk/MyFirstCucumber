a99bbf44a95172a16b2089d683d467ca16f5e5f1
Manas Kalenov <manasworks@gmail.com>
2022-03-11 22:04:41 -0600

    Initial commit

---
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -0,0 +1,56 @@
+package com.cydeo.utilities;
+
+/*
+In this class only general utility methods that are not related to some specific page.
+ */
+
+import org.junit.Assert;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.util.Set;
+
+public class BrowserUtils {
+
+    /*
+    This method will accept int (in seconds) and execute Thread.sleep
+    for given duration
+     */
+    public static void sleep(double second){
+
+        try {
+            Thread.sleep( (int)second*1000);
+        }catch (InterruptedException e ) {
+            e.printStackTrace();
+        }
+    }
+
+    // Method to find a window with expected URL
+    public static WebDriver switchWindowAndVerify(String expectedInUrl){
+
+        Set<String> allWindowsHandles = Driver.getDriver().getWindowHandles();
+
+        for (String each : allWindowsHandles) {
+            Driver.getDriver().switchTo().window(each);
+            if (Driver.getDriver().getCurrentUrl().contains(expectedInUrl)) break;
+        }
+        return Driver.getDriver();
+    }
+
+    public static void titleVerify(String expectedTitle){
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertEquals(actualTitle, expectedTitle);
+    }
+
+    public static void titleContains(String expectedTitle){
+        String actualTitle = Driver.getDriver().getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+    }
+
+    public static void scrollPage(int y, int x){
+        // Use below JavaScript method and scroll
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
+        js.executeScript("window.scrollBy("+y+","+x+")");
+    }
+
+}
